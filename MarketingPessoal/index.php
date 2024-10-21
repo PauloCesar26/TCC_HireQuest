da596e7f7a29f930b10058158c63ec528f8cbd9b
PauloCesar26 <pc.cordeirolima@gmail.com>
2024-10-21 16:07:09 -0300

    TCC_HireQuest

--- MarketingPessoal/index.php
+++ MarketingPessoal/index.php
@@ -37,7 +37,7 @@ $result = $conn->query($sql);
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
     <link rel="icon" href="img/currency-bitcoin.svg" type="image/x-icon">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style.css?v=1">
     <title>Marketing Pessoal</title>
 </head>
 <body>
